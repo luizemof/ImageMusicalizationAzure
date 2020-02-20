2b02ab4b11549fe5f8dc3f3fc5ee359f6cdb82ae
luizinhovao <luiz.emof@gmail.com>
2020-02-20 11:40:40 -0300

    Add functions into sln and reference ExtractionProject

--- src/Functions/ImageMusicalization.cs
+++ src/Functions/ImageMusicalization.cs
@@ -7,25 +7,33 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Extraction.KMeans;
 
 namespace Functions
 {
     public static class ImageMusicalization
     {
         [FunctionName("ImageMusicalization")]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, ILogger log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req, ILogger log)
         {
             log.LogInformation("Received message");
 
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            try
+            {
+                using (var extraction = new KMeansExtraction(null, 1))
+                {
+                    extraction.Run();
+                }
+
+                return new OkObjectResult($"Hello");
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
         }
     }
 }
