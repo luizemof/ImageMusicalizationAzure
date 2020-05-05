e99eb6a4b0d53ca32eea3c34ad07a3703b75e899
luizinhovao <luiz.emof@gmail.com>
2020-05-05 20:17:32 -0300

    Convert Note Enum value to string

--- src/Models/ImageMusicalization/ImageMusicalizationModel.cs
+++ src/Models/ImageMusicalization/ImageMusicalizationModel.cs
@@ -1,9 +1,12 @@
 using Models.NoteGeneration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Models.ImageMusicalization
 {
     public class ImageMusicalizationModel
     {
+        [JsonConverter(typeof(StringEnumConverter))]
         public ENote Note { get; set; }
     }
 }
